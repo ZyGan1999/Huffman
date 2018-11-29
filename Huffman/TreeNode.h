032b4b2ebb3ba7c9e27cedab0e2c5d128bbd2809
qiuk17 <qiuk17@mails.tsinghua.edu.cn>
2018-11-29 12:43:38 +0800

    Bug fixed

--- Huffman/TreeNode.h
+++ Huffman/TreeNode.h
@@ -1,4 +1,5 @@
 #pragma once
+
 class TreeNode {
 	char _char; int _freq;
 	TreeNode * child[2];
@@ -10,7 +11,17 @@ public:
 
 	TreeNode(TreeNode * mergeA, TreeNode * mergeB) : _char(0), _freq(mergeA->_freq + mergeB->_freq) {
 		mergeA->father = mergeB->father = this;
-		child[0] = (mergeA), child[1] = (mergeB);
+		child[0] = mergeA, child[1] = mergeB;
+	}
+
+	TreeNode(const TreeNode& copy) {
+		if (copy.child[0] != nullptr) copy.child[0]->father = this;
+		if (copy.child[1] != nullptr) copy.child[1]->father = this;
+		father = copy.father;
+		_freq = copy._freq;
+		_char = copy._char;
+		child[0] = copy.child[0];
+		child[1] = copy.child[1];
 	}
 
 	char getChar() const {
@@ -25,12 +36,13 @@ public:
 		return child[isRight];
 	}
 
-	TreeNode* getFather()  {
+	TreeNode* getFather() {
 		return father;
 	}
 
 	int isRightChild()  {
 		if (father == nullptr) return -1;
+		if (father == (void*)0xcccccccccccccccc) return -1;
 		return (this == father->child[1]);
 	}
 
